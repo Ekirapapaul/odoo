387757f7b69fec56bad647454a9400c5fde31bda
Jean Paul Mupagasi <jeanpaul.mupagasi@cureinternational.org>
2019-07-30 12:16:34 +0200

    [ADD] Watermark in report

--- addons/kijabe_purchase_ext/__manifest__.py
+++ addons/kijabe_purchase_ext/__manifest__.py
@@ -10,6 +10,7 @@
             'security/purchase_groups.xml',
             'security/ir.model.access.csv',
             'views/kijabe_purchase_ext_view.xml',
-            'views/internal_requisition_view.xml'
+            'views/internal_requisition_view.xml',
+            'report/report.xml'
         ],
 }
